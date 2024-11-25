688b0a7b54a43c3c5e5448eb5734f56451a541f7
Ean <86180758+EAN724@users.noreply.github.com>
2024-11-25 18:17:36 +0900

    asdf

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -6,7 +6,7 @@ public class BackgroundScroll : MonoBehaviour
 {
     Material mat;
     float cureentYoffset = 0f; //현재 Y축 오프셋(원래 위치에서 얼마나 떨어져 있는지에 대한 값)
-    float speed = 0.08f;
+    public float speed = 0.08f;
     
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/enemy/BulletMoveForward.cs
+++ Assets/Scripts/enemy/BulletMoveForward.cs
@@ -7,18 +7,10 @@ public class BulletMoveForward : MonoBehaviour
     public float speed = 5.0f; // �Ѿ� �ӵ�
     private Vector2 direction;  // �Ѿ��� �̵��� ����
 
-    private Rigidbody2D rb;
 
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
         Destroy(gameObject, 5f);
-
-    }
-
-    void FixedUpdate()
-    {
-        rb.velocity = direction * speed;
 
     }
 
